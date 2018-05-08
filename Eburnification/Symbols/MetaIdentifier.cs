ae0c9c6770bd009999498ebd06078d88296cbda5
Pascal Craponne <pascal@craponne.fr>
2018-05-08 22:45:53 +0200

    Fixed MetaIdentifier

--- Eburnification/Symbols/MetaIdentifier.cs
+++ Eburnification/Symbols/MetaIdentifier.cs
@@ -8,7 +8,7 @@ namespace Eburnification.Symbols
     {
         public override ParsingResult TryParse(Tokenizer tokenizer, Parser parser)
         {
-            if (!parser.TryRead(char.IsLetter))
+            if (!char.IsLetter(parser.Peek() ?? '\0'))
                 return ParsingResult.None;
             // which is actually always true, since we ask for as many as we wantd
             return tokenizer.ParseSequence(parser, MetaIdentifierCharacter.Instance, int.MaxValue);
